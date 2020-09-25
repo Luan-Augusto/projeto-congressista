df3df3bd307a54ee244e9e3b2a48791573d165d6
Luan-Augusto <luanalencar134@gmail.com>
2020-09-25 00:28:11 -0300

    instanciei obj Congressista

--- projeto-congressista/src/in/augustluan/entidades/Teste.java
+++ projeto-congressista/src/in/augustluan/entidades/Teste.java
@@ -1,9 +1,20 @@
 package in.augustluan.entidades;
 
+import java.util.HashMap;
+
 public abstract class Teste {
 
     public static void main(String args[]) {
 
-        Congressista congressista = new Congressista("Luan", 12345);
+        Congressista cong1 = new Congressista("Luan", 12345);
+        Congressista cong2 = new Congressista("Fulan", 54321);
+
+        HashMap<Congressista, String> hash = new HashMap<Congressista, String>();
+
+        // adicionando no hash
+        hash.put(cong1, "Informacao importante sobre " + cong1);
+        hash.put(cong2, "Informacao importante sobre " + cong2);
+
+        
     }
 }
